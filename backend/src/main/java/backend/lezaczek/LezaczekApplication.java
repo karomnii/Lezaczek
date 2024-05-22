75c2cff9559b794c81eea14d7f9270f95f5eb013
karomnii <ledwochp34@gmail.com>
2024-05-22 22:22:03 +0200

    Events REST Controller

--- backend/src/main/java/backend/lezaczek/LezaczekApplication.java
+++ backend/src/main/java/backend/lezaczek/LezaczekApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LezaczekApplication {
     public static void main(String[] args) {
+        System.setProperty("server.servlet.context-path", "/api/v1");
         SpringApplication.run(LezaczekApplication.class, args);
     }
 }
