fabf674154cedae15a7f4abab9b68d7435eb30a3
Natalia Balicka <245763@edu.p.lodz.pl>
2024-05-30 18:12:59 +0200

    deleted comments to include changes into main branch

--- backend/src/main/java/backend/lezaczek/Config/NewsConfig.java
+++ backend/src/main/java/backend/lezaczek/Config/NewsConfig.java
@@ -13,34 +13,4 @@ import java.util.List;
 
 @Configuration
 public class NewsConfig {
-    //this code inserts data on every run, so I commented that for now
-
-//    @Bean
-//    CommandLineRunner commandLineRunner(NewsRepository repository){
-//        return args -> {
-//            News weeia = new News(
-//                    2,
-//                    "Dni wydziału EEIA",
-//                    "Najlepsze wydarzenie na PŁ",
-//                    "WEEIA",
-//                    LocalDate.of(2024, Month.APRIL, 29),
-//                    LocalDate.of(2024, Month.MAY,31),
-//                    LocalTime.of(8,30),
-//                    LocalTime.of(16,0)
-//            );
-//            News aka = new News(
-//                    2,
-//                    "Targi pracy",
-//                    "Najlepsze wydarzenie na hali EXPO",
-//                    "Hala EXPO al. Politechniki",
-//                    LocalDate.of(2024, Month.APRIL, 4),
-//                    LocalDate.of(2024, Month.JUNE,3),
-//                    LocalTime.of(8,0),
-//                    LocalTime.of(18,0)
-//            );
-//            repository.saveAll(
-//                    List.of(weeia, aka)
-//            );
-//        };
-//    }
 }
