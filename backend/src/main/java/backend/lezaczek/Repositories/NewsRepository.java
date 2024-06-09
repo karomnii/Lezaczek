6d20aa76106e37f2e698a45d2f8339674ab1b22d
Natalia Balicka <245763@edu.p.lodz.pl>
2024-06-09 16:11:06 +0200

    added deletion of outdated news, provided checking isAdmin status, changed news update

--- backend/src/main/java/backend/lezaczek/Repositories/NewsRepository.java
+++ backend/src/main/java/backend/lezaczek/Repositories/NewsRepository.java
@@ -2,7 +2,6 @@ package backend.lezaczek.Repositories;
 
 import backend.lezaczek.Model.News;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +10,6 @@ import java.util.Optional;
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
     Optional<News> findNewsByName(String newsName);
-
-    List<News> findAllByOrderByDateOfEventAsc();
+    List<News> findAllByOrderByDateOfEventAscStartingTimeAsc();
+    void deleteNewsByNewsId(Long newsId);
 }
